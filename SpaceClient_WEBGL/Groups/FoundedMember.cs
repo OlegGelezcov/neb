af721f519c151fc1188f2d48c0b2d97225b01e13
OlegGelezcov <oleggelezcovs@gmail.com>
2015-12-05 23:54:24 +0300

    groups

---
+++ SpaceClient_WEBGL/Groups/FoundedMember.cs
@@ -0,0 +1,22 @@
+﻿using Common;
+using ServerClientCommon;
+using ExitGames.Client.Photon;
+using Nebula.Client.Utils;
+
+namespace Nebula.Client.Groups {
+    public class FoundedMember : IInfoParser {
+        public string characterID { get; private set; }
+        public Workshop workshop { get; private set; }
+        public int exp { get; private set; }
+
+        public FoundedMember(string characterID, Hashtable info) {
+            this.characterID = characterID;
+            ParseInfo(info);
+        }
+
+        public void ParseInfo(Hashtable info) {
+            workshop = (Workshop)(byte)info.GetValueInt((int)SPC.Workshop, (int)(byte)Workshop.Arlen);
+            exp = info.GetValueInt((int)SPC.Exp);
+        }
+    }
+}
